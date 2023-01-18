9c66a9acc68e7b0278c67cecd91511d0e7035966
Tanishq Mudaliar <tanishqmudaliar846@gmail.com>
2023-01-19 01:04:59 +0530

    Day 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import Mainpage from './Components/Mainpage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Mainpage />
     </div>
   );
 }
---
+++ src/Components/Mainpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../App.css";
+
+function Mainpage() {
+  return (
+    <div className='mainpage'>
+        Hi
+    </div>
+  )
+}
+
+export default Mainpage
