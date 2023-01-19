b1536aed3a015d1fb7ffdfc6a86da4a790a5a675
Tanishq Mudaliar <tanishqmudaliar846@gmail.com>
2023-01-20 03:09:35 +0530

    Day 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,106 @@
+import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Mainpage from './Components/Mainpage';
+import { Box, IconButton } from '@mui/material';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 function App() {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
   return (
     <div className="App">
-      <Header />
+      <Box
+        sx={{
+          position: 'fixed',
+          display: 'flex',
+          bgcolor: 'background.default',
+          height: 'fit-content',
+          width: '100vw',
+          padding: '1%',
+          borderBottom: '1px solid grey',
+        }}
+      >
+        <Header />
+        <div className="header_r">
+          <Box
+            sx={{
+              color: 'text.secondary'
+            }}
+          >
+            {theme.palette.mode} mode
+            <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+              {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+          </Box>
+        </div>
+      </Box>
       <Mainpage />
     </div>
   );
 }
 
-export default App;
+export default function ToggleColorMode() {
+  const [mode, setMode] = React.useState('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          background: {
+            ...(mode === 'dark'
+              ? {
+                default: '#131313'
+              } : {
+                default: '#edf1fd'
+              }
+            )
+          },
+          text: {
+            ...(mode === 'dark'
+              ? {
+                primary: '#ffb300',
+                secondary: 'rgba(255, 255, 255, 0.7)'
+              } : {
+                primary: '#000000',
+                secondary: '#303030'
+              }
+            )
+          },
+          icon: {
+            ...(mode === 'dark'
+              ? {
+                background: 'rgba(255, 255, 255, 0.7)',
+                color: '#303030'
+              } : {
+                background: '#303030',
+                color: '#ffb300'
+              }
+            )
+          },
+        },
+      }),
+    [mode],
+  );  
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
--- src/Components/Mainpage.js
+++ src/Components/Mainpage.js
@@ -1,10 +1,211 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../App.css";
+import html from "../Assets/html5.png";
+import css from "../Assets/css3.png";
+import js from "../Assets/javascript.png";
+import { Box, IconButton } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+import LogoDevIcon from '@mui/icons-material/LogoDev';
+
+const IconRedirectButton = styled(IconButton)({
+  margin: 5,
+  '&:hover': {
+    backgroundColor: '#ffb300',
+    color: '#131313',
+    borderRadius: '10px',
+  },
+});
 
 function Mainpage() {
+  const [ihover,setihover] = useState(false);
+  const [thover,setthover] = useState(false);
+  const [whover,setwhover] = useState(false);
+  const [ghover,setghover] = useState(false);
+  const [dhover,setdhover] = useState(false);
+  const [ehover,setehover] = useState(false);
   return (
-    <div className='mainpage'>
-        Hi
+    <div>
+        <Box
+          sx={{
+            marginTop: '6.3vh',
+            bgcolor: 'background.default',
+            display: 'flex',
+          }}
+        >
+          <div className='mainpage_ct'>
+            <Box
+              sx={{
+                mt: '25vh',
+                display: 'flex',
+              }}
+            >
+              <div className='sofia'>
+                <Box
+                  sx={{
+                    color: "text.secondary",
+                    fontSize: '35px',
+                  }}
+                >
+                  Hello, my name is
+                </Box>
+              </div>
+              <div className='pacifico'>
+                <Box
+                  sx={{
+                    fontSize: '40px',
+                    color: '#ffb300',
+                    mt: -2.7,
+                    ml: 1
+                  }}
+                >
+                  Tanishq Mudaliar
+                </Box>
+              </div>
+            </Box>
+            <Box
+              sx={{
+                mt: '5vh',
+                textAlign: 'left',
+              }}
+            >
+            <div className='sofia'>
+              <Box
+                sx={{
+                  fontSize: '20px',
+                  color: 'text.secondary'
+                }}
+              >
+                I'm a freelance web designer & full-stack developer from Mumbai, India.
+                I focus on writing clean, elegant, and efficient code.
+                I have an adequate experience with HTML5 <img src={html} alt="html5" className="icon" />,
+                CSS3 <img src={css} alt="css3" className="icon" />,
+                and Javascript <img src={js} alt="js" className="icon" /><br/>(Node JS & React JS).
+              </Box>
+            </div>
+            </Box>
+            <Box
+              sx={{
+                textAlign: 'left',
+                marginTop: '7vh',
+              }}
+            >
+              <IconRedirectButton
+                href="https://instagram.com/tanishq_mudaliar"
+                target="_blank"
+                rel="noopener"
+                onMouseOver={()=>setihover(true)} 
+                onMouseOut={()=>setihover(false)} 
+                sx={{
+                  backgroundColor: 'icon.background',
+                  fontFamily: 'Sofia Sans, sans-serif',
+                  color: 'icon.color',
+                  height: '50px',
+                  minWidth: '50px',
+                  fontSize: '20px',
+                }}
+              >
+                <InstagramIcon />
+                {ihover === true ? "Instagram" : ""}
+              </IconRedirectButton>
+              <IconRedirectButton
+                href="https://twitter.com/tanishqmudaliar"
+                target="_blank"
+                rel="noopener"
+                onMouseOver={()=>setthover(true)} 
+                onMouseOut={()=>setthover(false)}
+                sx={{
+                  backgroundColor: 'icon.background',
+                  fontFamily: 'Sofia Sans, sans-serif',
+                  color: 'icon.color',
+                  height: '50px',
+                  minWidth: '50px',
+                  fontSize: '20px',
+                }}
+              >
+                <TwitterIcon />
+                {thover === true ? "Twitter" : ""}
+              </IconRedirectButton>
+              <IconRedirectButton
+                href="https://wa.me/+917977188240"
+                target="_blank"
+                rel="noopener"
+                onMouseOver={()=>setwhover(true)} 
+                onMouseOut={()=>setwhover(false)}
+                sx={{
+                  backgroundColor: 'icon.background',
+                  fontFamily: 'Sofia Sans, sans-serif',
+                  color: 'icon.color',
+                  height: '50px',
+                  minWidth: '50px',
+                  fontSize: '20px',
+                }}
+              >
+                <WhatsAppIcon />
+                {whover === true ? "Whatsapp" : ""}
+              </IconRedirectButton>
+              <IconRedirectButton
+                href="https://github.com/tanishqmudaliar"
+                target="_blank"
+                rel="noopener"
+                onMouseOver={()=>setghover(true)} 
+                onMouseOut={()=>setghover(false)}
+                sx={{
+                  backgroundColor: 'icon.background',
+                  fontFamily: 'Sofia Sans, sans-serif',
+                  color: 'icon.color',
+                  height: '50px',
+                  minWidth: '50px',
+                  fontSize: '20px',
+                }}
+              >
+                <GitHubIcon />
+                {ghover === true ? "Github" : ""}
+              </IconRedirectButton>
+              <IconRedirectButton
+                href="https://dev.to/tanishqmudaliar"
+                target="_blank"
+                rel="noopener"
+                onMouseOver={()=>setdhover(true)} 
+                onMouseOut={()=>setdhover(false)}
+                sx={{
+                  backgroundColor: 'icon.background',
+                  fontFamily: 'Sofia Sans, sans-serif',
+                  color: 'icon.color',
+                  height: '50px',
+                  minWidth: '50px',
+                  fontSize: '20px',
+                }}
+              >
+                <LogoDevIcon />
+                {dhover === true ? "Dev.to" : ""}
+              </IconRedirectButton>
+              <IconRedirectButton
+                href="mailto:tanishqmudaliar1123@gmail.com"
+                target="_blank"
+                rel="noopener"
+                onMouseOver={()=>setehover(true)} 
+                onMouseOut={()=>setehover(false)}
+                sx={{
+                  backgroundColor: 'icon.background',
+                  fontFamily: 'Sofia Sans, sans-serif',
+                  color: 'icon.color',
+                  height: '50px',
+                  minWidth: '50px',
+                  fontSize: '20px',
+                }}
+              >
+                <EmailIcon />
+                {ehover === true ? "Email" : ""}
+              </IconRedirectButton>
+            </Box>
+          </div>
+          <div className='mainpage_bg' />
+        </Box>
     </div>
   )
 }
